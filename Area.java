ccfbb46e6650940a6c0f837deacd1c9ce5b21da5
prathmeshkokare <prathmeshkokare009@gmail.com>
2025-06-30 11:54:22 +0530

    Add files via upload

--- Area.java
+++ Area.java
@@ -1,4 +1,15 @@
+package demo01;
+
 public class Area {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         double len=20 , br=10 , area;
+         area=len*br;
+         System.out.println("Area :"+area);
+         System.out.println("length "+len+" breadth "+br);
+         System.out.printf("Area : %2f",area);
+         
+	}
 
 }
